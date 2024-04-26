50cd436d36d967c0060c245c040d57faf83e23a7
Suhas Khare <suhaskhare@gmail.com>
2024-04-26 14:28:40 +0530

    Mysql Generic Backup Script

--- Mysql/mysqlbackup.sh
+++ Mysql/mysqlbackup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This file was generated using the following command:
-# sudo mysql_config_editor set --login-path=client --user=root --host=localhost --password
-# Ensure strict permissions on .mylogin.cnf (e.g., 0600)
-MYSQL_LOGIN_FILE="/root/.mylogin.cnf"
+#
+# This script reads mysql credentials from $HOME/.mylogin.cnf file
+#
+
 
 # Get today's date in YYYY-MM-DD format
 TODAY=$(date +%Y-%m-%d)
@@ -15,8 +15,12 @@ mkdir -p "$BACKUP_DIR"
 # List all databases (excluding system databases)
 DATABASES=$(/usr/bin/mysql -Bse "SHOW DATABASES" | grep -E 'information_schema|performance_schema|^sys' -v)
 
-# Truncate error log
->/tmp/mysqldump_error.log
+# Check if databases list could be obtained
+if [ "${PIPESTATUS[0]}" -ne 0 ]
+then
+  echo "Could not get database list. Aborting."
+  exit 1
+fi
 
 # Loop through each database and create backup
 for database in $DATABASES; do
@@ -29,12 +33,12 @@ for database in $DATABASES; do
   fi
 
   # Dump database and compress with gzip
-  /usr/bin/mysqldump --defaults-extra-file="$MYSQL_LOGIN_FILE" "$database" 2>>/tmp/mysqldump_error.log | gzip > "$BACKUP_DIR/$TODAY-$database.sql.gz"
+  /usr/bin/mysqldump --defaults-extra-file="$MYSQL_LOGIN_FILE" "$database" | gzip > "$BACKUP_DIR/$TODAY-$database.sql.gz"
 
   # Check for mysqldump errors
   if [ "`echo ${PIPESTATUS[@]}`" != "0 0" ]
   then
-    echo "Error during backup of database $database. See /tmp/mysqldump_error.log for details."
+    echo "Error during backup of database $database"
     rm "$BACKUP_DIR/$TODAY-$database.sql.gz" 2>/dev/null
     exit 1
   fi
